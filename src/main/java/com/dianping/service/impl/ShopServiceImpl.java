de50f9e86157bdc71417589dda79cbf72f9587af
zhoujinxin12 <1640451907@qq.com>
2024-12-01 00:08:13 +0800

    upt

--- src/main/java/com/dianping/service/impl/ShopServiceImpl.java
+++ src/main/java/com/dianping/service/impl/ShopServiceImpl.java
@@ -9,10 +9,14 @@ import com.dianping.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.dianping.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.dianping.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -41,13 +45,28 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         }
         // 4.根据id查询数据库
         Shop shop = getById(id);
+        // 5.数据库中不存在返回错误
         if (shop == null) {
-            // 5.数据库中不存在返回错误
             return Result.fail("店铺不存在！");
         }
         // 6.存在，写入redis
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.expire(key, CACHE_SHOP_TTL, TimeUnit.MINUTES);
         // 7.返回
         return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        Long id = shop.getId();
+        if (id == null) {
+            return Result.fail("店铺id不能为空");
+        }
+        // 1.更新数据库
+        updateById(shop);
+        // 2.删除缓存
+        stringRedisTemplate.delete(CACHE_SHOP_KEY+id);
+        return Result.ok();
     }
 }
