91fa2b098d82a620b4a89f02f1727b59e27a31cb
zhoujinxin12 <1640451907@qq.com>
2024-12-03 23:48:22 +0800

    upt

--- src/main/java/com/dianping/service/impl/ShopServiceImpl.java
+++ src/main/java/com/dianping/service/impl/ShopServiceImpl.java
@@ -2,18 +2,24 @@ package com.dianping.service.impl;
 
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.dianping.dto.Result;
 import com.dianping.entity.Shop;
 import com.dianping.mapper.ShopMapper;
 import com.dianping.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.dianping.utils.CacheClient;
+import com.dianping.utils.RedisData;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.dianping.utils.RedisConstants.*;
@@ -32,12 +38,21 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    @Resource
+    private CacheClient cacheClient;
+
     @Override
     public Result queryById(Long id) {
         // 缓存穿透
-//        Shop shop = queryWithPassThrough(id);
+//        Shop shop = cacheClient.queryWithPassThrough(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
         // 互斥锁解决缓存击穿
-        Shop shop = queryWithMutex(id);
+//        Shop shop = queryWithMutex(id);
+//        if (shop == null) {
+//            return Result.fail("店铺不存在！");
+//        }
+        // 逻辑过期锁解决缓存击穿
+        Shop shop = cacheClient.queryWithLogicalExpire(CACHE_SHOP_KEY, id, Shop.class, this::getById, 20L, TimeUnit.SECONDS);
+
         if (shop == null) {
             return Result.fail("店铺不存在！");
         }
@@ -45,66 +60,60 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         return Result.ok(shop);
     }
 
-    public Shop queryWithMutex(Long id) {
-        String key = CACHE_SHOP_KEY + id;
-        // 1.从redis查询商铺缓存
-        String shopJson = stringRedisTemplate.opsForValue().get(key);
-        // 2.判断是否存在
-        if (StrUtil.isNotBlank(shopJson)) {
-            // 3.存在直接返回
-            return JSONUtil.toBean(shopJson, Shop.class);
-        }
-        // 判断命中的是否是空值
-        if (shopJson != null) {
-            // 返回一个错误信息
-            // 某个key再数据库中可能就是空
-            // 这个时候会再缓存中存储" ", "\t"等表示数据为空
-            return null;
-        }
+//
+//    public Shop queryWithMutex(Long id) {
+//        String key = CACHE_SHOP_KEY + id;
+//        // 1.从redis查询商铺缓存
+//        String shopJson = stringRedisTemplate.opsForValue().get(key);
+//        // 2.判断是否存在
+//        if (StrUtil.isNotBlank(shopJson)) {
+//            // 3.存在直接返回
+//            return JSONUtil.toBean(shopJson, Shop.class);
+//        }
+//        // 判断命中的是否是空值
+//        if (shopJson != null) {
+//            // 返回一个错误信息
+//            // 某个key再数据库中可能就是空
+//            // 这个时候会再缓存中存储" ", "\t"等表示数据为空
+//            return null;
+//        }
+//
+//        String lockKey = LOCK_SHOP_KEY + id;
+//        Shop shop = null;
+//        try {
+//            // 4.实现缓存重建
+//            // 4.1. 获取互斥锁
+//            boolean isLock = tryLock(lockKey);
+//            // 4.2. 判断是否获取成功
+//            if (!isLock) {
+//                // 4.3. 失败，则休眠重试
+//                Thread.sleep(50);
+//                queryWithMutex(id);
+//            }
+//
+//            // 4.4. 成功，根据id查询数据库
+//            shop = getById(id);
+//            // 模拟重建的延时.
+//            Thread.sleep(200);
+//            // 5.数据库中不存在返回错误
+//            if (shop == null) {
+//                // 将空值写入redis
+//                stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+//                return null;
+//            }
+//            // 6.存在，写入redis
+//            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+//            stringRedisTemplate.expire(key, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+//        } catch (InterruptedException e) {
+//            throw  new RuntimeException(e);
+//        } finally {
+//            // 7.释放互斥锁
+//            unlock(lockKey);
+//        }
+//        // 8.返回
+//        return shop;
+//    }
 
-        String lockKey = LOCK_SHOP_KEY + id;
-        Shop shop = null;
-        try {
-            // 4.实现缓存重建
-            // 4.1. 获取互斥锁
-            boolean isLock = tryLock(lockKey);
-            // 4.2. 判断是否获取成功
-            if (!isLock) {
-                // 4.3. 失败，则休眠重试
-                Thread.sleep(50);
-                queryWithMutex(id);
-            }
-
-            // 4.4. 成功，根据id查询数据库
-            shop = getById(id);
-            // 模拟重建的延时.
-            Thread.sleep(200);
-            // 5.数据库中不存在返回错误
-            if (shop == null) {
-                // 将空值写入redis
-                stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
-                return null;
-            }
-            // 6.存在，写入redis
-            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
-            stringRedisTemplate.expire(key, CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        } catch (InterruptedException e) {
-            throw  new RuntimeException(e);
-        } finally {
-            // 7.释放互斥锁
-            unlock(lockKey);
-        }
-        // 8.返回
-        return shop;
-    }
-
-    private boolean tryLock(String key) {
-        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
-        return BooleanUtil.isTrue(flag);
-    }
-    private void unlock(String key) {
-        stringRedisTemplate.delete(key);
-    }
     @Override
     @Transactional
     public Result update(Shop shop) {
---
+++ src/main/java/com/dianping/utils/RedisIdWorker.java
@@ -0,0 +1,43 @@
+package com.dianping.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+
+    private static final long BEGIN_TIMESTAMP = 1704067200L;
+    /**
+     * 序列号的位数
+     */
+    private static final int COUNT_BITS = 32;
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public long nextId(String keyPrefix) {
+        // keyPrefix是业务的前缀
+        // 1. 生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+        // 2. 生成序列号
+        // 2.1. 获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        // 3. 拼接并返回
+        return timestamp << COUNT_BITS | count ;
+    }
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.of(2024, 1, 1, 0, 0, 0);
+        long second = time.toEpochSecond(ZoneOffset.UTC);
+        System.out.println("second=" + second);
+    }
+}
