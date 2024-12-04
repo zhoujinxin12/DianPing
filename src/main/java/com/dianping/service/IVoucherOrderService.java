d8060ea87c46193fb994062dda889213c0806bf4
zhoujinxin12 <1640451907@qq.com>
2024-12-04 14:01:41 +0800

    upt

--- src/main/java/com/dianping/service/IVoucherOrderService.java
+++ src/main/java/com/dianping/service/IVoucherOrderService.java
@@ -1,5 +1,6 @@
 package com.dianping.service;
 
+import com.dianping.dto.Result;
 import com.dianping.entity.VoucherOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IVoucherOrderService extends IService<VoucherOrder> {
 
+    Result seckillVoucher(Long voucherId);
 }
--- src/main/java/com/dianping/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/dianping/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,19 @@
 package com.dianping.service.impl;
 
+import com.dianping.dto.Result;
+import com.dianping.entity.SeckillVoucher;
 import com.dianping.entity.VoucherOrder;
 import com.dianping.mapper.VoucherOrderMapper;
+import com.dianping.service.ISeckillVoucherService;
 import com.dianping.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.dianping.utils.RedisIdWorker;
+import com.dianping.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +26,52 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        // 1. 查询
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        // 2. 判断秒杀是否开始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            // 尚未开始
+            return Result.fail("秒杀尚未开始！");
+        }
+        // 3. 判断秒杀是否结束
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            // 已经结束
+            return Result.fail("秒杀已经结束！");
+        }
+        // 4. 判断库存是否充足
+        if (voucher.getStock() < 1) {
+            // 库存不足
+            return Result.fail("库存不足！");
+        }
+        // 5. 扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId).update();
+        if (!success) {
+            // 扣减失败
+            return Result.fail("库存不足！");
+        }
+        // 6. 创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        // 6.1. 订单id
+        long id = redisIdWorker.nextId("order");
+        voucherOrder.setId(id);
+        // 6.2. 用户id
+        Long userId = UserHolder.getUser().getId();
+        voucherOrder.setUserId(userId);
+        // 6.3. 代金券id
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+        // 7. 返回订单id
+        return Result.ok(voucherOrder);
+    }
 }
